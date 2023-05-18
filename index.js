de8708382520ab87ca4bbd8482725fadf6bb1c3d
monsur776e <monsuralam8897@gmail.com>
2023-05-19 02:38:22 +0600

    middleware set success

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
    res.send("teddyBear server is running");
 });
+
 app.listen(port, () => {
    console.log(`server listening on port ${port}`);
 });
